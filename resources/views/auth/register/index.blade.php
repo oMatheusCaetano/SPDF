047602ab0d32192ccf9f25104f1ca9a82ded2727
Matheus Caetano <caetanomatheus876@gmail.com>
2020-04-18 12:45:27 -0300

    Rota e View de Registro de usuários

---
+++ resources/views/auth/register/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('window_title') Registrar-se | S.PDF @endsection
+@section('page_content')
+    <div class="m-5">
+        <div class="container col-lg-5 p-2 bg-white shadow">
+            <form class="bg-white shadow p-5" action="">
+                <div>
+                    <h2 class="text-center">Registrar-se</h2>
+                    <h6 class="text-center mb-3">Registrar-se no S.PDF</h6>
+                </div>
+                <div class="form-group">
+                    <label for="name">Nome</label>
+                    <input class="form-control" id="name" name="name" placeholder="Informe o seu nome...">
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input class="form-control" id="email" name="email" type="email" placeholder="Informe o seu e-mail...">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" id="password" name="password" type="password" placeholder="Informe a senha de acesso...">
+                </div>
+                <div class="form-group">
+                    <label for="password_confirmation">Repetir Senha</label>
+                    <input class="form-control" id="password_confirmation" name="password_confirmation" type="password" placeholder="Repita a senha de acesso...">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-info">Registrar-se</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">S.PDF</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="{{ route('register.index') }}">Registrar-se</a>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </nav>
+</header>
